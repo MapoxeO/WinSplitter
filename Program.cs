0a67238ab6f2450cd0557c35f65b02fc6098ded6
MapoxeO <92392962+MapoxeO@users.noreply.github.com>
2021-12-14 00:58:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MapoxeO.Utils.FileSpliters {
+    class Program {
+        unsafe static void Main(string[] args) {
+            var path = "test.bin";
+            BinFileSplitter bfs = new BinFileSplitter(path);
+            bfs.Split(5, true);
+        }
+    }
+}
