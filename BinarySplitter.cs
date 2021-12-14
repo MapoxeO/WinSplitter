5f4700e52f89b2aad21dd11ed48bdef34c03f242
MapoxeO <92392962+MapoxeO@users.noreply.github.com>
2021-12-15 00:53:22 +0500

    Новая версия
    Теперь данный класс работает в ~100% случаев. Делит файлы, размерами до 2147483647 байт ( чуть меньше 2 ГБ )

--- BinarySplitter.cs
+++ BinarySplitter.cs
@@ -1,58 +1,50 @@
-using System;
+﻿using System;
 using System.IO;
-using System.Collections.Generic;
 
 namespace MapoxeO {
 	public class BinarySplitter {
-		public string FilePath { get; private set; }
-		public List<string>? OutPathes { get; private set; } = null;
-		
+		public string FilePath { get; private set; } = null;
+
 		public BinarySplitter(string filePath) {
 			FilePath = filePath;
 		}
-		
-		public void Split(uint bytes, bool createDirectory = true) {
-			if (File.Exists(FilePath) == false)
+
+		public int Split(int bytes, bool createDirectory = true) {
+			if(File.Exists(FilePath) == false)
 				throw new Exception($"File in path {FilePath} doesn't exist");
 			var finfo = new FileInfo(FilePath);
-			uint num_size = finfo.Size / bytes;
-			uint ost = finfo.Size % bytes;
-			
-			OutPathes = new List<string>(num_size + ost);
-			
-			string newPath = null;
-			string directoryPath = null;
-			if (createDirectory) {
-				directoryPath = $"{Environment.CurrentDirectory}\\{FilePath} - Splitted";
-				Directory.CreateDirectory(directoryPath);
-			} else {
-				newPath = $"{FilePath.Enviroment}\\";
+			long num_size = finfo.Length / bytes;
+			long ost = finfo.Length % bytes;
+
+			string newPath = $"{Environment.CurrentDirectory}";
+			if(createDirectory) {
+				newPath += $"\\{finfo.Name} - Splitted";
+				Directory.CreateDirectory(newPath);
 			}
-			
+
 			try {
-				using (var fin = BinaryReader(File.Open(FilePath, FileMode.Open))) {
-					if (num_size != 0) {
-						for (uint i = 0; i < num_size; i++) {
-							string fp = $"{directoryPath}\\{FilePath}-{i + 1}";
-							OutPathes.Append(fp);
-							using (var fout = new BinaryReader(File.Open(fp, FileMode.CreateNew)) {
+				using(var fin = new BinaryReader(File.Open(FilePath, FileMode.Open))) {
+					if(num_size != 0) {
+						for(long i = 0; i < num_size; i++) {
+							using (var fout = new BinaryWriter(File.Open($"{newPath}\\{finfo.Name} - {i}.spt", FileMode.CreateNew))) {
 								fout.Write(fin.ReadBytes(bytes));
 							}
 						}
 					}
-					if (ost != 0) {
-						string fp = $"{directoryPath}\\{FilePath}-{num_size}";
-						OutPathes.Append(fp);
-						using (var fout = new BinaryReader(File.Open(fp, FileMode.CreateNew))) {
-							fout.Write(fin.ReadBytes(ost));
+
+					if(ost != 0) {
+						using (var fout = new BinaryWriter(File.Open($"{newPath}\\{finfo.Name} - {num_size}.spt", FileMode.CreateNew))) {
+							fout.Write(fin.ReadBytes((int) ost));
 						}
 					}
 				}
-			} catch (Exception e) {
-				if (Directory.Exists(directoryPath))
-					Directory.Delete(directoryPath);
-				throw new Exception(e.What);
 			}
+			catch(Exception e) {
+				if(Directory.Exists(newPath))
+					Directory.Delete(newPath);
+				throw new Exception(e.Message);
+			}
+			return (int) num_size + (ost > 0 ? 1 : 0);
 		}
 	}
 }
