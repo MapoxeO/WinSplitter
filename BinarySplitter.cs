deaf24366013bbd6a266d74be40640fac4487151
MapoxeO <92392962+MapoxeO@users.noreply.github.com>
2021-12-15 21:22:58 +0500

    Обновлённые файлы

--- BinarySplitter.cs
+++ BinarySplitter.cs
@@ -6,14 +6,17 @@ namespace MapoxeO {
 		public string FilePath { get; private set; } = null;
 
 		public BinarySplitter(string filePath) {
-			if (!File.Exists(filePath))
-				throw new Exception($"File in path {filePath} doesn't exist");
+			Exists(filePath);
 			FilePath = filePath;
 		}
 
+		private void Exists(in string filePath) {
+			if(!File.Exists(filePath))
+				throw new Exception($"File in path {filePath} doesn't exist");
+		}
+
 		public int Split(int bytes, bool createDirectory = true) {
-			if(File.Exists(FilePath) == false)
-				throw new Exception($"File in path {FilePath} doesn't exist");
+			Exists(FilePath);
 			var finfo = new FileInfo(FilePath);
 			long num_size = finfo.Length / bytes;
 			long ost = finfo.Length % bytes;
@@ -28,15 +31,15 @@ namespace MapoxeO {
 				using(var fin = new BinaryReader(File.Open(FilePath, FileMode.Open))) {
 					if(num_size != 0) {
 						for(long i = 0; i < num_size; i++) {
-							using (var fout = new BinaryWriter(File.Open($"{newPath}\\{finfo.Name} - {i}.spt", FileMode.CreateNew))) {
+							using(var fout = new BinaryWriter(File.Open($"{newPath}\\{finfo.Name} - {i}.spt", FileMode.CreateNew))) {
 								fout.Write(fin.ReadBytes(bytes));
 							}
 						}
 					}
 
 					if(ost != 0) {
-						using (var fout = new BinaryWriter(File.Open($"{newPath}\\{finfo.Name} - {num_size}.spt", FileMode.CreateNew))) {
-							fout.Write(fin.ReadBytes((int) ost));
+						using(var fout = new BinaryWriter(File.Open($"{newPath}\\{finfo.Name} - {num_size}.spt", FileMode.CreateNew))) {
+							fout.Write(fin.ReadBytes((int)ost));
 						}
 					}
 				}
@@ -46,7 +49,7 @@ namespace MapoxeO {
 					Directory.Delete(newPath);
 				throw new Exception(e.Message);
 			}
-			return (int) num_size + (ost > 0 ? 1 : 0);
+			return (int)num_size + (ost > 0 ? 1 : 0);
 		}
 	}
 }
--- BinaryUniter.cs
+++ BinaryUniter.cs
@@ -4,41 +4,44 @@ using System.IO;
 namespace MapoxeO {
 	public class BinaryUniter {
 		public string[] Pathes { get; private set; } = null;
-		public static const string DefaultFileExtension = ".spt";
-		
-		public delegate void WarningHandler(string path);
+		public string FileOut { get; private set; } = null;
+		public static string DefaultFileExtension { get; } = ".spt";
+
+		public delegate void WarningHandler(string path, string cause);
 		public event WarningHandler Warning;
-		
+
 		public BinaryUniter(params string[] pathes) {
-			Exists();
 			Pathes = pathes;
+			Exists();
 		}
-		
+
 		private void Exists() {
-			for (int i = 0; i < Pathes.Length; i++) {
-				if (!File.Exists(Pathes[i]))
+			for(int i = 0; i < Pathes.Length; i++) {
+				if(!File.Exists(Pathes[i]))
 					throw new Exception($"File in path {Pathes[i]} doesn't exist.");
-				if (Pathes[i].Substring(Pathes[i].Length - 4) != DefaultFileExtension)
-					WarningHandler?.Invoke(path);
+				if(Pathes[i].Substring(Pathes[i].Length - 4) != DefaultFileExtension)
+					Warning?.Invoke(Pathes[i], "file is in incorrect extension");
 			}
 		}
-		
+
 		public void Unite(string fileName = "outUniter.bin") {
 			Exists();
-			string newPath = Enviroment.CurrentDirectory;
-			
+			string newPath = Environment.CurrentDirectory;
+
 			try {
-				using (var fout = new BinaryWriter(File.Open($"{newPath}\\{fileName}", FileMode.Append))) {
-					for(int i = 0; i < FilesPaths.Length; i++) {
-						using (var fin = new BinaryReader(File.Open(FilesPaths[i], FileMode.Open))) {
-							var finfo = new FileInfo(FilesPaths[i]);
-							fout.Write(fin.ReadBytes((int) finfo.Length);
+				using(var fout = new BinaryWriter(File.Open($"{newPath}\\{fileName}", FileMode.Create))) {
+					for(int i = 0; i < Pathes.Length; i++) {
+						using(var fin = new BinaryReader(File.Open(Pathes[i], FileMode.Open))) {
+							var finfo = new FileInfo(Pathes[i]);
+							fout.Write(fin.ReadBytes((int) finfo.Length));
 						}
 					}
 				}
-			} catch (Exception e) {
-				throew new Exception(e.Message);
 			}
+			catch(Exception e) {
+				throw new Exception(e.Message);
+			}
+			FileOut = $"{newPath}\\{fileName}";
 		}
 	}
 }
